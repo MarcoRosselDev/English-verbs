e25b0022c0edcdd8fa54d7a75ad60e14646038d6
Marco Rossel <andresmarcorossel@gmail.com>
2026-09-26 04:17:43 -0300

    frontend vite with react and typescript | day 5 | deploying

---
+++ backend-fastapi/api/index.py
@@ -0,0 +1,5 @@
+# backend-fastapi/api/index.py
+from src.verb_api.main import app
+
+# Vercel busca una variable llamada 'app' para pasarla al servidor ASGI.
+# La importación de arriba cumple con ese requisito.
